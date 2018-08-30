84941f6a73fd2112f74fe55e9cb91fc866e42375
Christy Cui <christycui14@gmail.com>
2018-08-29 21:59:12 -0400

    back

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -89,7 +89,14 @@ class TLDetector(object):
         """
         self.has_image = True
         self.camera_image = msg
-        light_wp, state = self.process_traffic_lights()
+        if self.process_light_count < 15:
+            self.process_light_count += 1
+            light_wp, state = self.last_wp, self.state
+        else:
+            light_wp, state = self.process_traffic_lights()
+            light_wp_code = light_wp if state == TrafficLight.RED else -1
+            self.process_light_pub.publish(Int32(light_wp_code))
+            self.process_light_count = 0
 	# collect data to build classifier
         #self.save_img_to_file(msg)
 	'''
@@ -137,15 +144,9 @@ class TLDetector(object):
         if(not self.has_image):
             self.prev_light_loc = None
             return False
-        if self.process_light_count < 30:
-            self.process_light_count += 1
-            return self.state
-        else:
-            cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-            state = self.light_classifier.get_classification(cv_image)
-            self.process_light_pub.publish(Int32(state))
-            self.process_light_count = 0
-            return state
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        state = self.light_classifier.get_classification(cv_image)
+        return state
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
